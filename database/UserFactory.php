cbe58cbcc32c9791ebb3a9309991937a301482e7
Ryuz11 <132235853+Ryuz11@users.noreply.github.com>
2025-07-01 03:36:55 +0700

    Add files via upload

---
+++ database/UserFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserFactory extends Factory
+{
+    // Kosongkan agar tidak mengganggu seeder
+}
